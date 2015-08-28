97c69cd4c3c812385d97d32d817a0cc655a68488
craigthelinguist <aaroncraig@protonmail.ch>
2015-08-29 00:17:52 +1200

    Register enter key on player.
    Console now prints out when you push enter key

--- FruitSeller/Assets/Scripts/Inventory.cs
+++ FruitSeller/Assets/Scripts/Inventory.cs
@@ -1,1 +1,16 @@
 
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	public List<Item> inventory = new List<Item>();
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			Debug.Log ("Enter Key pressed");
+		}
+	}
+
+}
