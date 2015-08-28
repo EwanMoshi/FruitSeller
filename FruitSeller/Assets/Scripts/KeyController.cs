d61baa40bcff4c72aae9773d8ad693e6dadeb558
craigthelinguist <aaroncraig@protonmail.ch>
2015-08-29 01:59:48 +1200

    Add inventory button
    Pressing 'tab' brings up an inventory overlay

--- FruitSeller/Assets/Scripts/KeyController.cs
+++ FruitSeller/Assets/Scripts/KeyController.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Inventory : MonoBehaviour {
-
-	public List<Item> inventory = new List<Item>();
-
+public class KeyController : MonoBehaviour {
+	
+	public InventoryGUI inventoryGUI;
+	
 	void Update() {
 		if (Input.GetKeyDown (KeyCode.Return)) {
 			Debug.Log ("Enter Key pressed");
+		} else if (Input.GetKeyDown (KeyCode.Tab)) {
+			inventoryGUI.toggleInventory();
 		}
 	}
-
+	
 }
