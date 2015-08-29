7d12aadd6a58a8a6a32286a9ef2be16ed4c99cc8
nealhartley <hartleneal@myvuw.ac.nz>
2015-08-30 02:30:55 +1200

    slaying dayone

--- FruitSeller/Assets/Scripts/mannequinspawn.cs
+++ FruitSeller/Assets/Scripts/mannequinspawn.cs
@@ -6,6 +6,9 @@ public class mannequinspawn : MonoBehaviour {
 	public Transform spawnPoint;
 	public GameObject prefab;
 	private Vector3 spawned;
+
+	private bool hasPlayedEnter = false;
+	private bool hasPlayedExit = false;
 	// Use this for initialization
 	void Start () {
 		spawned = spawnPoint.position;
@@ -18,12 +21,29 @@ public class mannequinspawn : MonoBehaviour {
 
 	void OnTriggerExit(Collider other){
 
-		if (other.gameObject.tag == "player") {
+		if (other.gameObject.tag == "player" && !hasPlayedExit) {
 		
 			Instantiate(prefab, spawned, transform.localRotation);
-		
+
+			spawnPoint.GetComponent<AudioSource>().Play();
+		    
+			hasPlayedExit = true;
 		}
 
+
+	}
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.gameObject.tag == "player" && !hasPlayedEnter) {
+			
+			AudioSource source = GetComponent<AudioSource>();
+
+			source.Play();
+
+			hasPlayedEnter =true;
+			
+		}
 
 	}
 }
