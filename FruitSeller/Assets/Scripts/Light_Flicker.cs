1eff20762d88181a6867e217410a5e79598cda73
nealhartley <hartleneal@myvuw.ac.nz>
2015-08-29 18:29:33 +1200

    lighting for upstairs beginnings

---
+++ FruitSeller/Assets/Scripts/Light_Flicker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Light_Flicker : MonoBehaviour {
+
+
+	//private bool on = true;
+	public Light theLight;
+	public bool flickered = true;
+	// Use this for initialization
+	void Start () {
+	
+		theLight = GetComponent<Light>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (!flickered) {
+			theLight.enabled = !theLight.enabled;
+			flickered= true;
+		} else {
+			flickered = false;
+		}
+	}
+}
