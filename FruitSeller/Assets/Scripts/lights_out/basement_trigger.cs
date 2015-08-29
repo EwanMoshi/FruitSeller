aec54909b244fd948092c69585d391c1723c8c90
nealhartley <hartleneal@myvuw.ac.nz>
2015-08-30 11:48:55 +1200

    lights out scene

---
+++ FruitSeller/Assets/Scripts/lights_out/basement_trigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basement_trigger : MonoBehaviour {
+
+	public Light turnOff;
+	private bool onePlay = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		print ("entered trigger");
+
+		if (onePlay) {
+			print ("turning light off");
+			turnOff.enabled = !turnOff.enabled;
+			onePlay = false;
+		}
+
+	}
+}
---
+++ FruitSeller/Assets/Scripts/lights_out/light_off.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class light_off : MonoBehaviour {
+
+	//add all lights to this array you want off at start of day
+
+	public Light[] lights;
+
+
+	// Use this for initialization
+	void Start () {
+
+		foreach(Light lt in lights){
+
+			lt.enabled = !lt.enabled;
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
