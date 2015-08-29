6bbda38ec81a1904c91dda9b5970fdaa4bd8af54
craigthelinguist <aaroncraig@protonmail.ch>
2015-08-30 03:18:18 +1200

    Add interactive hints.

--- FruitSeller/Assets/Scripts/InteractiveBehaviour.cs
+++ FruitSeller/Assets/Scripts/InteractiveBehaviour.cs
@@ -2,8 +2,13 @@
 using System.Collections;
 
 public class InteractiveBehaviour : MonoBehaviour {
-
+	
+	// Description to use when you interact with this.
 	public string interactiveDescription;
+	
+	// How long to display the interactive description.
 	public float timeToDisplay;
-
+	
+	// What to display when you're standing in front of this.
+	public string hint;
 }
--- FruitSeller/Assets/Scripts/clickOnShit.cs
+++ FruitSeller/Assets/Scripts/clickOnShit.cs
@@ -2,34 +2,34 @@ using UnityEngine;
 using System.Collections;
 
 public class clickOnShit : MonoBehaviour {
-
+	
 	public AudioClip pickupSound;
 	public AudioClip paperSound;
-
+	
 	public float range = 2f;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-
+	
 	bool interactive (string tag) {
 		return tag.Equals ("interactive") || tag.Equals ("item + interactive");
 	}
-
+	
 	bool pickupable (string tag) {
 		return tag.Equals ("item") || tag.Equals ("item + interactive");
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		
 		if (Input.GetKeyDown(KeyCode.E)) {
 			Ray ray = Camera.main.ScreenPointToRay(new Vector3( Screen.width / 2, Screen.height / 2, 0));
 			RaycastHit hit;
 			Debug.Log ("Hello");
-
+			
 			if (Physics.Raycast(ray, out hit)) {
-
+				
 				/*
 				Debug.Log ("Name = " + hit.collider.name);
 				Debug.Log ("Tag = " + hit.collider.tag);
@@ -38,10 +38,10 @@ public class clickOnShit : MonoBehaviour {
 				Debug.Log ("--------------");
 				*/
 				if(hit.collider.tag.Equals("switch") && hit.distance < range){
-				
+					
 					LightSwitch light = hit.collider.gameObject.GetComponent<LightSwitch>();
 					light.clicked();
-
+					
 				}
 				
 				if (interactive(hit.collider.tag) && hit.distance < range) {
@@ -49,25 +49,25 @@ public class clickOnShit : MonoBehaviour {
 					InteractiveBehaviour ib = obj.GetComponent<InteractiveBehaviour>();
 					InventoryGUI.SetInteractiveDisplay(ib.interactiveDescription, ib.timeToDisplay);
 				}
-
+				
 				if (pickupable(hit.collider.tag) && hit.distance < range){
 					GameObject obj = hit.collider.gameObject;
 					ItemBehaviour item = obj.GetComponent<ItemBehaviour>();
-
+					
 					// Attempt to pick up item. If successful, remove item from game world.
 					// Play pick-up sound.
-
+					
 					if (InventoryGUI.hasSpace()) {
 						InventoryGUI.addItem(item);
 						AudioSource.PlayClipAtPoint(pickupSound, Camera.main.transform.position);
 						Destroy(obj);
 					}
-
+					
 				}
-
+				
 			}
 		}
 	}
-
-
+	
+	
 }
