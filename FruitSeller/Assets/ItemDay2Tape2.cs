289fe2edc06cfbc81785200a348f8289c94c2501
nealhartley <hartleneal@myvuw.ac.nz>
2015-08-30 15:52:51 +1200

    Add day 2 tape 2

---
+++ FruitSeller/Assets/ItemDay2Tape2.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class ItemDay2Tape2 : ItemHandler {
+	
+	public AudioClip sound2play;
+	public GameObject radio;
+	public int range = 2;
+	
+	// What to do when this item gets used.
+	public override void Use() {
+		AudioSource.PlayClipAtPoint(sound2play, Camera.main.transform.position);
+	}
+	
+	public override bool Consumes() {
+		return true;
+	}
+	
+	public override bool Usable() {
+		return Vector3.Distance (Camera.main.transform.position, radio.transform.position) < range;
+	}
+	
+}
