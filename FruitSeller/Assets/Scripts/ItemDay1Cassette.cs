1cc978a84e12104fc4d2ce995f9ff6677afa4448
ewanmoshi@live.com <ewanmoshi@live.com>
2015-08-30 15:15:29 +1200

    static noise quieter when playing tapes

--- FruitSeller/Assets/Scripts/ItemDay1Cassette.cs
+++ FruitSeller/Assets/Scripts/ItemDay1Cassette.cs
@@ -2,13 +2,22 @@
 using System.Collections;
 
 public class ItemDay1Cassette : ItemHandler {
-	
+
+	public AudioSource staticNoise;
 	public AudioClip sound2play;
 	public GameObject radio;
 	public int range = 2;
-	
+
+	public void Start() {
+		print ("started");
+		GameObject obj = GameObject.FindGameObjectWithTag ("StaticNoise");
+		staticNoise = obj.GetComponent<AudioSource>();
+	}
+
 	// What to do when this item gets used.
 	public override void Use() {
+		staticNoise.volume = 0f;
+		if(staticNoise==null){print ("fugg");}
 		AudioSource.PlayClipAtPoint(sound2play, Camera.main.transform.position);
 	}
 	
