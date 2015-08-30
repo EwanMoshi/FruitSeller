98d9d140286df294c85b3de5ae0a8e4b7207a8f5
craigthelinguist <aaroncraig@protonmail.ch>
2015-08-30 14:41:32 +1200

    Add tutorial

---
+++ FruitSeller/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+
+	public float delay1 = 2f;
+	public GameObject obj;
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > delay1) {
+			InventoryGUI.SetInteractiveDisplay("Look at things. Press E to interact with them.", 3);
+			Destroy (obj);
+		}
+	}
+}
