dc411d07f5cefdef317b89b94985e180693c7e68
nealhartley <hartleneal@myvuw.ac.nz>
2015-08-30 12:53:51 +1200

    dont turn off the lights more shite

---
+++ FruitSeller/Assets/Scripts/lights_out/enterkitchentrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enterkitchentrigger : MonoBehaviour {
+
+
+	public AudioSource playThis;
+	private bool hasPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		print ("walked into kitch");
+		if(other.gameObject.tag == "Player" && !hasPlayed){
+			print ("hear that scream?");
+			playThis.Play();
+			hasPlayed = true;
+		}
+
+	}
+}
