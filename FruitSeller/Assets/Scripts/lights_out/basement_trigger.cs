7adc8df16fa3f821bee936c2471f2915cb9c017c
nealhartley <hartleneal@myvuw.ac.nz>
2015-08-30 12:38:15 +1200

    dont look back!

--- FruitSeller/Assets/Scripts/lights_out/basement_trigger.cs
+++ FruitSeller/Assets/Scripts/lights_out/basement_trigger.cs
@@ -4,7 +4,16 @@ using System.Collections;
 public class basement_trigger : MonoBehaviour {
 
 	public Light turnOff;
+	public Light torch;
+	public GameObject mannequin;
 	private bool onePlay = true;
+	private bool timed = false;
+
+	public float timeToWait = 2f;
+	public float timer = 0;
+
+	private bool waited = true;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -13,16 +22,38 @@ public class basement_trigger : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if(timed){
+			timer += Time.deltaTime;
+			if(timer > timeToWait && waited){
+				torch.enabled = !torch.enabled;
+				waited = false;
+			}
+
+		}
+
 	}
 
 	void OnTriggerEnter(Collider other){
+		if(onePlay){
+		torch.enabled = !torch.enabled;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
 		print ("entered trigger");
 
 		if (onePlay) {
 			print ("turning light off");
 			turnOff.enabled = !turnOff.enabled;
+
+			timed = true;
+			//torch.enabled = !torch.enabled;
+
+			Destroy(mannequin);
 			onePlay = false;
 		}
 
 	}
+
+
 }
